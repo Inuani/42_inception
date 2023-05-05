4e64771bc026014a9255a05c96d584e6f704420f
Elie Gauthey <egauthey@c2r4s1.42lausanne.ch>
2023-05-05 12:07:49 +0200

    last version

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -5,7 +5,6 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
-# needed ?
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
